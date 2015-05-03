e232d103da4c68d3110c98dfd1446f8f0eca21ab
albert <albertdom.ba2@gmail.com>
2015-05-04 01:10:04 +0200

    Login.html added

--- gestiohorts/gestiohorts/urls.py
+++ gestiohorts/gestiohorts/urls.py
@@ -1,13 +1,29 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
-from myhorts.views import *
+
+#from myhorts.views import *
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gestiohorts.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^myhorts/',  include('myhorts.urls',  namespace='myhorts')),
+    url(r'^myhorts/', include('myhorts.urls', namespace='myhorts')),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, }),
+        )

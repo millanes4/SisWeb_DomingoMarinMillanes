fd9851718b6df02f960de025000805e963d97667
albert <albertdom.ba2@gmail.com>
2015-05-04 01:20:23 +0200

    Login.html added

--- gestiohorts/myhorts/views.py
+++ gestiohorts/myhorts/views.py
@@ -2,14 +2,42 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from django.core import serializers
+
+from django.utils import timezone
+
 from django.core import urlresolvers
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
+from django.views.generic.base import TemplateResponseMixin
 from django.views.generic.edit import CreateView
 
 from models import Arbre, Propietari, Hort
 from forms import HortForm, ArbreForm
+
+
+class ConnegResponseMixin(TemplateResponseMixin):
+    def render_json_object_response(self, objects, **kwargs):
+        json_data = serializers.serialize(u"json", objects, **kwargs)
+        return HttpResponse(json_data, content_type=u"application/json")
+
+    def render_xml_object_response(self, objects, **kwargs):
+        xml_data = serializers.serialize(u"xml", objects, **kwargs)
+        return HttpResponse(xml_data, content_type=u"application/xml")
+
+    def render_to_response(self, context, **kwargs):
+        if 'extension' in self.kwargs:
+            try:
+                objects = [self.object]
+            except AttributeError:
+                objects = self.object_list
+            if self.kwargs['extension'] == 'json':
+                return self.render_json_object_response(objects=objects)
+            elif self.kwargs['extension'] == 'xml':
+                return self.render_xml_object_response(objects=objects)
+        else:
+            return super(ConnegResponseMixin, self).render_to_response(context)
 
 
 class HortDetail(DetailView):
@@ -20,6 +48,14 @@ class HortDetail(DetailView):
         context = super(HortDetail, self).get_context_data(**kwargs)
         return context
 
+
+class HortList(ListView, ConnegResponseMixin):
+    model = Hort
+    queryset = Hort.objects.filter(date__lte=timezone.now()).order_by('date')[:5]
+    context_object_name = 'latest_hort_list'
+    template_name = 'myhorts/hort_list.html'
+
+
 class HortCreate(CreateView):
     model = Hort
     template_name = 'myhorts/form.html'
@@ -28,6 +64,7 @@ class HortCreate(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(HortCreate, self).form_valid(form)
+
 
 class ArbreCreate(CreateView):
     model = Arbre
